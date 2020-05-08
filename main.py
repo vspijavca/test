623ed2cea3d275059538afc07864c67dbe78ec10
Spijavca Veaceslav <dreamersf@gmail.com>
2020-05-08 23:29:13 +0300

    New commit4.

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-(1024).to_bytes(2, byteorder='big')
-(1024).to_bytes(10, byteorder='big')
-(-1024).to_bytes(10, byteorder='big', signed=True)
-x = 1000
-x.to_bytes((x.bit_length() // 8) + 1, byteorder='little')
+float.fromhex('0x1.5000000000000p+3')
