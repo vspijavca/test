9c18d4db4042635057e68e884aa829355d18283e
Spijavca Veaceslav <dreamersf@gmail.com>
2020-05-08 23:20:26 +0300

    New commit3.

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 (1024).to_bytes(2, byteorder='big')
+(1024).to_bytes(10, byteorder='big')
+(-1024).to_bytes(10, byteorder='big', signed=True)
+x = 1000
+x.to_bytes((x.bit_length() // 8) + 1, byteorder='little')
