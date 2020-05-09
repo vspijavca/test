19237c89b33265f22ea761e5181c0a9b31f6e0eb
Spijavca Veaceslav <dreamersf@gmail.com>
2020-05-09 09:00:03 +0300

    Comment9.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 'Hello, {}!'.format('Vasya')
 "Units destroyed: {players[0]}".format(players = [1, 2, 3])
-'{}, {}, {}'.format('a', 'b', 'c')
+ 'Coordinates: {latitude}, {longitude}'.format(latitude='37.24N', longitude='-115.81W')
+'Coordinates: 37.24N, -115.81W'
